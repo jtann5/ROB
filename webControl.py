83f7126b469ea041215918f994528232303ac002
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-02-26 21:55:31 -0700

    Added the webcontrol stuff
    Commented out the rob import since windows doesn't like linux only packages

--- webControl.py
+++ webControl.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template, send_from_directory, jsonify, request, Blueprint, redirect
 import os
-from rob import ROB
+# from rob import ROB
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'), static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/update', methods=['POST'])
+def update():
+    data = request.get_json()
+    joystickX = data.get("joystickX")
+    joystickY = data.get("joystickY")
+    slider1 = data.get('slider1')
+    slider2 = data.get('slider2')
+    slider3 = data.get('slider3')
+
+    print("Joystick X: " + str(joystickX))
+    print("Joystick Y: " + str(joystickY))
+    print(slider1, slider2, slider3)
+
+    response_data = {'status': 'success'}
+    return jsonify(response_data)
+
+def getWheelsValue(value):
+    print("v", str(value))
+    return str(value)
 
 app.run(debug=True, host="0.0.0.0", port=5000)
