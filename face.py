cce85c506807f96fdff7f0e7758542807c6f7e69
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-03-18 16:30:20 -0600

    Update face.py

--- face.py
+++ face.py
@@ -29,7 +29,7 @@ class RobotFace:
 
 
         self.mouth_size = 256
-        self.mouth_height = 485
+        self.mouth_height = 465
         self.mouthfirstcoord = (self.screen_width / 2) + (self.mouth_size / 2)
         self.mouthsecondcoord = self.mouthfirstcoord - self.mouth_size
         self.mouth_coords = [self.mouthsecondcoord, self.mouth_height, self.mouthfirstcoord, self.mouth_height]
