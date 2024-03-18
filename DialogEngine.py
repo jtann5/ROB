1b6c3b77948e76ddec8b505536b3bacc1a94d38d
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-03-18 14:01:16 -0600

    Update DialogEngine.py

--- DialogEngine.py
+++ DialogEngine.py
@@ -25,13 +25,40 @@ class DialogEngine:
     def openFile(self):
         file = open(self.DialogFile)
         self.src_code = file.read()
+        try:
+            index = 0
+            while index < len(self.src_code):
+                # consumes comments
+                if self.match("#"):
+                    while not self.match("\n"):
+                        self.consume()
+
+        except EOFError as e:
+            print(e)
 
     def match(self, input):
-        pass
+        if self.src_code[self.lineOffset] == input:
+            return True
+        return False
+
+    def matchandconsume(self, input):
+        if self.match(input):
+            self.lineOffset += 1
+            return True
+        return False
+
+    def consume(self):
+        self.lineOffset += 1
 
     def print(self):
         # self.src_code[0..] is the character at the index
-        print(self.src_code)
+        try:
+            index = 0
+            while index < len(self.src_code):
+                print(self.src_code[index])
+                index += 1
+        except EOFError as e:
+            print(e)
 
 
 if __name__ == "__main__":
