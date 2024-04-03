59f64e43bfcc642eb4eec313cd5b8e78f56d88fd
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-03 10:53:58 -0600

    Create speechtype.py

---
+++ speechtype.py
@@ -0,0 +1,31 @@
+from DialogEngine import DialogEngine
+from rob import rob
+from threading import Thread
+
+def run_speaking():
+    d = DialogEngine()
+    d.setFile('dialogInput.txt')
+    d.openFile()
+    running = True
+
+    while running:
+        print("Enter text: ")
+        word = input(">>> ")
+        if word.strip() == "bye":
+            break
+        output = d.analyze(word.strip())
+        rob.say(output)
+        print("Robot: " + str(output))
+
+
+
+if __name__ == "__main__":
+    speaking_thread = Thread(target=run_speaking)
+    face_thread = Thread(target=rob.start_face)
+
+    speaking_thread.start()
+    face_thread.start()
+    speaking_thread.join()
+    face_thread.join()
+
+
