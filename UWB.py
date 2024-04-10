19f49f0c0991c46cc876188344c1dfadbad34d5d
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-04-10 16:48:34 -0600

    Update UWB.py

--- UWB.py
+++ UWB.py
@@ -1,22 +1,21 @@
 import serial
 import time
 
-x = 0
 while True:
     ser = serial.Serial('/dev/ttyUSB0', 115200)
     ser.readline() # example mc 00 000001ab 00000d7f 0000124c 00000d31 2996 97 000f9bda t7:0 1a38
     response = ser.readline()  # example $KT7,0.45,3.43,4.70,3.36,LO=[no solution]
     #print(response.decode().strip())  # Decode bytes to string and remove newline characters
     arr = response.decode().strip().split(',')
-    float_array = [float(x) for x in arr[1:4]] 
+    float_array = [float(x) for x in arr[1:5]] 
     print("A0: " + str(float_array[0]) + "m")
     print("A1: " + str(float_array[1]) + "m")
     print("A2: " + str(float_array[2]) + "m")
     print("A3: " + str(float_array[3]) + "m")
     print("Quad: " + str(float_array.index(min(float_array))))
+    print("")
     ser.close()
-    if x > 100:
+        
+    if input().lower() == "exit":
         break
-    else:
-        x+=1
-        time.sleep(0.5)
+
