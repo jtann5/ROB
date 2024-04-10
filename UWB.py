07f0235d903acc7efa26832a94e48bbc95e5445d
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-04-10 16:29:26 -0600

    Update UWB.py

--- UWB.py
+++ UWB.py
@@ -5,13 +5,14 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)
 
 x = 0
 while True:
-    response = ser.readline()
+    ser.readline()
+    response = ser.readline() 
     print(response.decode().strip())  # Decode bytes to string and remove newline characters
     if x > 20:
         break
     else:
         x+=1
-        time.sleep(2)
+        time.sleep(0.5)
 
 # Close serial connection
 ser.close()
