e132d8cc75adf47ee1cb1c098c9a9a8b3ba5d828
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-04-10 16:51:41 -0600

    Update UWB.py

--- UWB.py
+++ UWB.py
@@ -5,6 +5,7 @@ while True:
     ser = serial.Serial('/dev/ttyUSB0', 115200)
     ser.readline() # example mc 00 000001ab 00000d7f 0000124c 00000d31 2996 97 000f9bda t7:0 1a38
     response = ser.readline()  # example $KT7,0.45,3.43,4.70,3.36,LO=[no solution]
+    print(response)
     #print(response.decode().strip())  # Decode bytes to string and remove newline characters
     arr = response.decode().strip().split(',')
     float_array = [float(x) for x in arr[1:5]] 
