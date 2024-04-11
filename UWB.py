9c579fbb397de3c7ac1b175ffc7a1605f870a56a
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-11 14:43:28 -0600

    Update UWB.py

--- UWB.py
+++ UWB.py
@@ -49,7 +49,7 @@ def calcPosition(coords):
     secondClosestAnchor = coords.index(min(coords))
     coords[closestAnchor] = original_val
 
-    x_coord = ((distance ** 2) - (secondClosestAnchor ** 2) + (closestAnchor ** 2)) / (2 * distance)
+    x_coord = ((distance ** 2) - (closestAnchor ** 2) + (secondClosestAnchor ** 2)) / (2 * distance)
     y_coord = math.sqrt(abs((closestAnchor ** 2) - (x_coord ** 2)))
 
     return x_coord, y_coord
