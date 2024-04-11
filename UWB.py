e625fa669b015b4a466fe49c45b42c4f7d331b96
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-11 15:29:59 -0600

    Update UWB.py

--- UWB.py
+++ UWB.py
@@ -170,9 +170,17 @@ if __name__ == "__main__":
     while ((not heading.robposx < 0 and not heading.robposx > 3) or (not heading.robposy < 0 and not heading.robposy > 3)):
         heading.printValues()
         if heading.robposx < 0 or heading.robposx > 3:
-            break
+            rob.setMotor(0, 5000)
+            rob.setMotor(1, 7000)
+            time.sleep(1)
+            rob.setMotor(0, 6000)
+            rob.setMotor(1, 6000)
         if heading.robposy < 0 or heading.robposy > 3:
-            break
+            rob.setMotor(0, 5000)
+            rob.setMotor(1, 7000)
+            time.sleep(1)
+            rob.setMotor(0, 6000)
+            rob.setMotor(1, 6000)
         #time.sleep(5)
         if heading.robProduct < 0:
             rob.setMotor(0, 5000)
