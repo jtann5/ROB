b35afc9062dc16158cdaf4e6f7b8779c0bd1dcf2
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-11 16:17:39 -0600

    Update UWB.py

--- UWB.py
+++ UWB.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
             rob.setMotor(1, 6000)
             break
         #time.sleep(5)
-        if heading.robProduct < 0.3:
+        if heading.robProduct < -0.28:
             rob.setMotor(0, 5000)
             rob.setMotor(1, 7000)
             time.sleep(1.25)
