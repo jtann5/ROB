63f92d7825b81ec7f5eaf8993e454467c9171112
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-23 17:27:09 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from rob import rob
 
+
 class BlockInstruction:
     def __init__(self):
         self.distance = 0
@@ -22,9 +23,6 @@ class BlockInstruction:
         self.wait_human_speech = False
         self.talking = False
         self.input = ""
-
-        ## ...
-        ## make the rest of them
 
     def set_movement(self, distance, speed, angle, direction, both_motors, left, right):
         pass
@@ -46,35 +44,29 @@ class BlockInstruction:
         print("rob is talking")
 
     def humanspeech(self):
-        print("I am listenign blahdlajsf lsj")
+        print("I am listening")
 
     def bodyturn(self):
         # send value to rob
-        print("robs body is turning")
+        rob.setMotor(2, self.waist_value)
 
     def headturn(self):
         # send pan value to rob
-        print("robs head is turning")
+        print("rob's head is turning")
+        rob.setMotor(4, self.head_pan_value)
 
     def headtilt(self):
         # send tilt to rob
-        print("Robs head is tilting")
+        print("rob's head is tilting")
+        rob.setMotor(3, self.head_tilt_value)
 
     def robotturn(self):
-        print("Rob is turning")
+        print("rob is turning")
 
     def movement(self):
         # this is where we send that stuff to rob
         print("There is movement")
 
-## icon instructions as follows
-## 0 Motors
-## 1 HEADTILT
-## 2 HEADTURN
-## 3 BODYTURN
-## 4 PAUSE
-## 5 ROBOTTURN
-## 6 TALKING includes waiting for human speed input, type sentence and 4 prebuilt sayings
 
 icon_instructions = []
 
@@ -88,8 +80,20 @@ icon_description = [
     "talk"
 ]
 
+icon_title = [
+    "Robot Movement",
+    "Robot Turn",
+    "Robot Head Tilt",
+    "Robot Head Turn",
+    "Robot Waist Turn",
+    "Wait for Human Speech",
+    "Robot Talking"
+]
+
+default_BlockInstructions = []
 
 execution_instructions = []
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -99,16 +103,6 @@ class Application(tk.Frame):
         self.icon_images = []  # Initialize icon_images attribute
         self.create_widgets()
 
-        self.icon_options = {
-            "icon1": ["Option1", "Option2", "Option3", "Option4"],
-            "icon2": ["Option1", "Option2", "Option3"],
-            "icon3": ["Option1", "Option2", "Option3"],
-            "icon4": ["Option1", "Option2", "Option3"],
-            "icon5": ["Option1", "Option2", "Option3"],
-            "icon6": ["Option1", "Option2", "Option3"],
-            "icon7": ["Option1", "Option2", "Option3"]
-        }
-
     def create_widgets(self):
         # Create the 7 icons on the right side of the screen
         self.icon_frame = tk.Frame(self.master)
@@ -116,13 +110,13 @@ class Application(tk.Frame):
 
         for i in range(7):
             # Load the icon image
-            image = Image.open(f"icon{i+1}.png")
+            image = Image.open(f"icon{i + 1}.png")
             image_dim = (60, 60)
             photo = ImageTk.PhotoImage(image.resize(image_dim))  # Resize the icon image
 
             self.icon_images.append(photo)
 
-            icon_label = tk.Label(self.icon_frame, image=photo, width=image_dim[0]+15, height=image_dim[1])
+            icon_label = tk.Label(self.icon_frame, image=photo, width=image_dim[0] + 15, height=image_dim[1])
             icon_label.image = photo  # Keep a reference to the image object to prevent it from being garbage collected
             icon_label.pack(side="top", fill="x")
 
@@ -136,9 +130,10 @@ class Application(tk.Frame):
 
             options_label = tk.Label(self.icon_frame, image=options_photo, width=20, height=20)
             options_label.image = options_photo
+            default_BlockInstructions.append(BlockInstruction())
 
             # Calculate the coordinates to position the options icon label at the top-right corner of the icon label
-            icon_width = image_dim[0]+15
+            icon_width = image_dim[0] + 15
             options_width = 20
             x_coord = icon_width - options_width
             y_coord = image_dim[1] - options_width
@@ -146,7 +141,7 @@ class Application(tk.Frame):
             options_label.place(in_=icon_label, x=x_coord, y=y_coord)
 
             # Bind the left mouse button click event to the options icon
-            options_label.bind("<Button-1>", self.options_command)
+            options_label.bind("<Button-1>", lambda event, i=i: self.options_command(i))
 
         # Create the 8 execution squares in the center of the screen
         self.square_frame = tk.Frame(self.master)
@@ -162,11 +157,11 @@ class Application(tk.Frame):
         button_frame.pack(side="bottom", padx=10, pady=10, anchor="se")
 
         # Create a clear button to clear all the squares
-        clear_button = tk.Button(self.master, text="Clear", command=self.clear_command, width=10, height=3)
+        clear_button = tk.Button(button_frame, text="Clear", command=self.clear_command, width=10, height=3)
         clear_button.pack(side="right", padx=5)
 
-        run_button = tk.Button(self.master, text="Run", command=self.run_command, width=10, height=3)
-        run_button.pack(side="bottom", padx=5)
+        run_button = tk.Button(button_frame, text="Run", command=self.run_command, width=10, height=3)
+        run_button.pack(side="right", padx=5)
 
         self.square_frame.pack(side="top", expand=True, fill="both")
 
@@ -174,36 +169,89 @@ class Application(tk.Frame):
         # this is where I go through the stuff and execute things
         for i in range(len(execution_instructions)):
             if execution_instructions[i] == "movement":
-                icon_instructions[i].movement(icon_instructions[i])
+                icon_instructions[i].movement()
             elif execution_instructions[i] == "robotturn":
-                icon_instructions[i].robotturn(icon_instructions[i])
+                icon_instructions[i].robotturn()
             elif execution_instructions[i] == "headtilt":
-                icon_instructions[i].headtilt(icon_instructions[i])
+                icon_instructions[i].headtilt()
             elif execution_instructions[i] == "headturn":
-                icon_instructions[i].headturn(icon_instructions[i])
+                icon_instructions[i].headturn()
             elif execution_instructions[i] == "bodyturn":
-                icon_instructions[i].bodyturn(icon_instructions[i])
+                icon_instructions[i].bodyturn()
             elif execution_instructions[i] == "humanspeech":
-                icon_instructions[i].humanspeech(icon_instructions[i])
+                icon_instructions[i].humanspeech()
             elif execution_instructions[i] == "talk":
-                icon_instructions[i].talk(icon_instructions[i])
+                icon_instructions[i].talk()
             else:
                 print("Invalid")
         rob.defaults()
 
     def icon_command(self, i):
-        print(f"Icon {i+1} pressed!")
+        print(f"Icon {i + 1} pressed!")
         # Move the icon to the first available square
         for square in self.squares:
             if not square.cget("image"):  # Check if the square is empty
                 square.config(image=self.icon_images[i], width=80, height=80)
                 execution_instructions.append(icon_description[i])
-                icon_instructions.append(BlockInstruction)
+                icon_instructions.append(default_BlockInstructions[i])
                 print(execution_instructions)
                 break
 
-    def options_command(self, event):
-        print("Options icon pressed!")
+    def options_command(self, icon_num, default=None):
+        print(icon_num)
+        self.settings_window = tk.Toplevel(self.master)
+        self.settings_window.title(f"{icon_title[icon_num]} Settings")
+        if default is not None:
+            pass
+        else:
+            if icon_description[icon_num].__eq__("movement"):
+                ## TODO movement
+                pass
+            elif icon_description[icon_num].__eq__("robotturn"):
+                ## TODO robotturn
+                pass
+            elif icon_description[icon_num].__eq__("headtilt"):
+                initialValue = default_BlockInstructions[icon_num].head_pan_value
+                self.headtilt_slider = tk.Scale(self.settings_window, from_=4000, to=8000, orient="horizontal")
+                self.headtilt_slider.set(initialValue)
+                self.headtilt_slider.pack()
+                apply_button = tk.Button(self.settings_window, text="Apply", command=self.apply_headtilt_settings_default)
+                apply_button.pack()
+            elif icon_description[icon_num].__eq__("headturn"):
+                initialValue = default_BlockInstructions[icon_num].head_pan_value
+                self.headturn_slider = tk.Scale(self.settings_window, from_=4000, to=8000, orient="horizontal")
+                self.headturn_slider.set(initialValue)
+                self.headturn_slider.pack()
+                apply_button = tk.Button(self.settings_window, text="Apply", command=self.apply_headturn_settings_default)
+                apply_button.pack()
+            elif icon_description[icon_num].__eq__("bodyturn"):
+                initialValue = default_BlockInstructions[icon_num].waist.value
+                self.waist_slider = tk.Scale(self.settings_window, from_=4000, to=8000, orient="horizontal")
+                self.waist_slider.set(initialValue)
+                self.waist_slider.pack()
+                apply_button = tk.Button(self.settings_window, text="Apply", command=self.apply_waist_settings_default)
+                apply_button.pack()
+            elif icon_description[icon_num].__eq__("humanspeech"):
+                ## TODO humanspeech
+                pass
+            elif icon_description[icon_num].__eq__("talk"):
+                ## TODO talk
+                pass
+
+    def apply_headtilt_settings_default(self):
+        headtilt_value = self.headtilt_slider.get()
+        default_BlockInstructions[2].set_headtilt(headtilt_value)
+        self.settings_window.destroy()
+
+    def apply_headturn_settings_default(self):
+        headturn_value = self.headturn_slider.get()
+        default_BlockInstructions[3].set_headturn(headturn_value)
+        self.settings_window.destroy()
+
+    def apply_waist_settings_default(self):
+        waist_value = self.waist_slider.get()
+        default_BlockInstructions[4].set_waist(waist_value)
+        self.settings_window.destroy()
 
     def clear_command(self):
         print("Clear button pressed!")
@@ -214,32 +262,6 @@ class Application(tk.Frame):
         for square in self.squares:
             square.config(image='', width=11, height=5)
 
-
-class OptionsPopup(tk.Toplevel):
-    def __init__(self, parent, icon_name, options):
-        super().__init__(parent)
-        self.parent = parent
-        self.icon_name = icon_name
-        self.options = options
-        self.selected_option = tk.StringVar()
-        self.selected_option.set(options[0] if options else "")
-
-        self.title(f"Options for {self.icon_name}")
-        self.geometry("200x150")
-
-        label = ttk.Label(self, text=f"Options for {self.icon_name}")
-        label.pack(pady=10)
-
-        options_menu = ttk.OptionMenu(self, self.selected_option, *options)
-        options_menu.pack(pady=5)
-
-        apply_button = ttk.Button(self, text="Apply", command=self.apply_options)
-        apply_button.pack(pady=5)
-
-    def apply_options(self):
-        selected_option = self.selected_option.get()
-        print(f"Selected option for {self.icon_name}: {selected_option}")
-        # Add your logic to apply the selected option
 
 
 root = tk.Tk()
