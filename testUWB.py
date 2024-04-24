ff7c5cacea0bbc09f7f558a9a725f99856ddf07a
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-04-24 11:11:23 -0600

    Update testUWB.py

--- testUWB.py
+++ testUWB.py
@@ -6,6 +6,7 @@ def readSerial():
     ser.readline()  # example mc 00 000001ab 00000d7f 0000124c 00000d31 2996 97 000f9bda t7:0 1a38
     response = ser.readline()  # example $KT7,0.45,3.43,4.70,3.36,LO=[no solution]
     while response.decode().strip().split(',')[0] != "$KT7":
+        print(response)
         response = ser.readline()
     print(response)
     # print(response.decode().strip())  # Decode bytes to string and remove newline characters
