aa856bbe766796145e196356d35cbfddaeb2f72f
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-04-25 18:16:00 -0600

    asdjkl;

--- speech.py
+++ speech.py
@@ -45,7 +45,7 @@ def get_response(text):
     return run.status
   
 def run_speaking():
-    rob.say(get_response("Generate a speech"))
+    rob.sayThread(get_response("Generate a speech"))
     while (rob.face.robot_state == 'talking'):
       rob.randomMovement()
 
