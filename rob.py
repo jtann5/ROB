89786f8d1df8cb9f5952262fd4a41fe3ca1c9d7d
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-28 10:39:57 -0600

    Update rob.py

--- rob.py
+++ rob.py
@@ -331,9 +331,9 @@ class ROB:
 
     def secondstocentimetersforwardbackward(self, centimeters, speed, direction):
         ## conversion factor is seconds/centimeters this can change to a function if we graph the amount of power
-        inches = int(centimeters/2.54)
-        print(inches)
-        conversionFactor = 1 / (30 + 3*inches)
+        ft = int(centimeters/2.54)/12
+        print(ft)
+        conversionFactor = 1 / (30 + 7*ft)
         amt_time = centimeters * conversionFactor
 
         if direction == "forward":
