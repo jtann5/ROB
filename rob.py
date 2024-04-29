a9cf434242cde4f709ad62636ca40f5242b2c609
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-28 19:28:33 -0600

    Update rob.py

--- rob.py
+++ rob.py
@@ -335,7 +335,7 @@ class ROB:
         ft = int(centimeters/2.54)/12
         ##print(ft)
         ##conversionFactor = -0.00501636 * (math.log(ft - 0.948925) / 1) + math.exp(-3.77479)
-        conversionFactor = 0.0175
+        conversionFactor = 0.0175 *2
         print(conversionFactor)
         amt_time = centimeters * conversionFactor
 
@@ -344,7 +344,7 @@ class ROB:
             rob.setMotor(1, 6000 + (speed * 20))
             time.sleep(amt_time)
         else:
-            rob.setMotor(0, 6000 + int(speed * 19))
+            rob.setMotor(0, 6000 + int(speed * 20))
             rob.setMotor(1, 6000 - (speed * 20))
             time.sleep(amt_time)
         rob.setMotor(0, 6000)
