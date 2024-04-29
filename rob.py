9836c36cb7dc48214860ff6ce18433e89a42c17a
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-04-28 20:02:38 -0600

    Update rob.py

--- rob.py
+++ rob.py
@@ -335,12 +335,12 @@ class ROB:
         ft = int(centimeters/2.54)/12
         ##print(ft)
         ##conversionFactor = -0.00501636 * (math.log(ft - 0.948925) / 1) + math.exp(-3.77479)
-        conversionFactor = 0.01146
+        conversionFactor = 0.01149
         print(conversionFactor)
         amt_time = centimeters * conversionFactor
 
         if direction == "forward":
-            rob.setMotor(0,  6000 - int(speed * 19.4))
+            rob.setMotor(0,  6000 - int(speed * 19.35))
             rob.setMotor(1, 6000 + (speed * 20))
             time.sleep(amt_time)
         else:
