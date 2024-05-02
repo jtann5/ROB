64f4e232df35cfd8817803e696e815d1e08d3cb0
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-05-02 15:23:57 -0600

    Update rob.py

--- rob.py
+++ rob.py
@@ -417,7 +417,7 @@ rob = ROB()
 
 if __name__ == "__main__":
     rob = ROB()
-    rob.turnDegrees(270)
+    rob.turnDegrees(90)
     #rob.sayThread("Hello")
     #for i in range(4):
     #    rob.sayThread("You get the joke")
