bbe57abb954cd291b37e0ea0e6f14b981dc463f0
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-05-02 13:48:07 -0600

    Update final.py

--- final.py
+++ final.py
@@ -367,7 +367,7 @@ def getRobProduct(type):
     roborienx1, roborieny1 = calcPosition(anchors, rob1)
     type.roborienx1 = roborienx1
     type.roborieny1 = roborieny1
-    robVectorX, robVectorY = vectorDetector(type.robXm, type.robYm, type.roborienx1, type.roborieny1)
+    robVectorY, robVectorX = vectorDetector(type.robXm, type.robYm, type.roborienx1, type.roborieny1)
     type.robVectorX = robVectorX * 10
     type.robVectorY = robVectorY * 10
 
