f1a4cf918bbbb4141ff85e6763bb6af3327b86d5
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-05-01 18:03:29 -0600

    Update rob.py

--- rob.py
+++ rob.py
@@ -393,7 +393,7 @@ class ROB:
             rob.setMotor(1, 6000 - speed)
             time.sleep(degrees * factor)
         else:
-            factor = 0.02
+            factor = 0.015
             rob.setMotor(0, 6000 + speed)
             rob.setMotor(1, 6000 + speed)
             time.sleep((360 - degrees) * factor)
